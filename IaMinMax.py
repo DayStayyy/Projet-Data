1d1b718e6256a62cfa5843848c1ce0e99e2c018c
DayStayyy <daystayyy@gmail.com>
2023-04-03 14:49:39 +0200

    add: save bests move

--- IaMinMax.py
+++ IaMinMax.py
@@ -1,14 +1,15 @@
+import os
 import chess
 import chess.svg
 import chess.polyglot
 from IPython.display import SVG
 import chess.pgn
-import datetime
 import chess.engine
+import pickle
 
 pawntable = [
     0, 0, 0, 0, 0, 0, 0, 0,
-    5, 10, 10, -20, -20, 10, 10, 5,
+    5, 10, 10, -20, -20, 10, 10, 5, 
     5, -5, -10, 0, 0, -10, -5, 5,
     0, 0, 0, 20, 20, 0, 0, 0,
     5, 5, 10, 25, 25, 10, 5, 5,
@@ -66,108 +67,152 @@ kingstable = [
     -30, -40, -40, -50, -50, -40, -40, -30,
     -30, -40, -40, -50, -50, -40, -40, -30]
 
+class ChessMinMax:
 
-def evaluate_board(board):
-    if board.is_checkmate():
+    def __init__(self, depth):
+        self.depth = depth
+        self.movehistory = {}
+        for i in range(1,depth+1):
+            with open(f'moveHistoryDepth.pickle', 'rb') as handle:
+                self.movehistory = pickle.load(handle)
+                
+    def evaluate_board(self,board):
+        if board.is_checkmate():
+            if board.turn:
+                return -9999
+            else:
+                return 9999
+        if board.is_stalemate():
+            return 0
+        if board.is_insufficient_material():
+            return 0
+
+        wp = len(board.pieces(chess.PAWN, chess.WHITE))
+        bp = len(board.pieces(chess.PAWN, chess.BLACK))
+        wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+        bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+        wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+        bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+        wr = len(board.pieces(chess.ROOK, chess.WHITE))
+        br = len(board.pieces(chess.ROOK, chess.BLACK))
+        wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+        bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+        material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+
+        pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+        pawnsq = pawnsq + sum([-pawntable[chess.square_mirror(i)]
+                            for i in board.pieces(chess.PAWN, chess.BLACK)])
+        knightsq = sum([knightstable[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+        knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)]
+                                for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+        bishopsq = sum([bishopstable[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+        bishopsq = bishopsq + sum([-bishopstable[chess.square_mirror(i)]
+                                for i in board.pieces(chess.BISHOP, chess.BLACK)])
+        rooksq = sum([rookstable[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
+        rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)]
+                            for i in board.pieces(chess.ROOK, chess.BLACK)])
+        queensq = sum([queenstable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
+        queensq = queensq + sum([-queenstable[chess.square_mirror(i)]
+                                for i in board.pieces(chess.QUEEN, chess.BLACK)])
+        kingsq = sum([kingstable[i] for i in board.pieces(chess.KING, chess.WHITE)])
+        kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)]
+                            for i in board.pieces(chess.KING, chess.BLACK)])
+
+        eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
         if board.turn:
-            return -9999
+            return eval
         else:
-            return 9999
-    if board.is_stalemate():
-        return 0
-    if board.is_insufficient_material():
-        return 0
-
-    wp = len(board.pieces(chess.PAWN, chess.WHITE))
-    bp = len(board.pieces(chess.PAWN, chess.BLACK))
-    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
-    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
-    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
-    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
-    wr = len(board.pieces(chess.ROOK, chess.WHITE))
-    br = len(board.pieces(chess.ROOK, chess.BLACK))
-    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
-    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
-
-    material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
-
-    pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
-    pawnsq = pawnsq + sum([-pawntable[chess.square_mirror(i)]
-                           for i in board.pieces(chess.PAWN, chess.BLACK)])
-    knightsq = sum([knightstable[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
-    knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)]
-                               for i in board.pieces(chess.KNIGHT, chess.BLACK)])
-    bishopsq = sum([bishopstable[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
-    bishopsq = bishopsq + sum([-bishopstable[chess.square_mirror(i)]
-                               for i in board.pieces(chess.BISHOP, chess.BLACK)])
-    rooksq = sum([rookstable[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
-    rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)]
-                           for i in board.pieces(chess.ROOK, chess.BLACK)])
-    queensq = sum([queenstable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
-    queensq = queensq + sum([-queenstable[chess.square_mirror(i)]
-                             for i in board.pieces(chess.QUEEN, chess.BLACK)])
-    kingsq = sum([kingstable[i] for i in board.pieces(chess.KING, chess.WHITE)])
-    kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)]
-                           for i in board.pieces(chess.KING, chess.BLACK)])
-
-    eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
-    if board.turn:
-        return eval
-    else:
-        return -eval
+            return -eval
 
 
-def alphabeta(alpha, beta, depthleft,board):
-    bestscore = -9999
-    if (depthleft == 0):
-        return quiesce(alpha, beta,board)
-    for move in board.legal_moves:
-        board.push(move)
-        score = -alphabeta(-beta, -alpha, depthleft - 1,board)
-        board.pop()
-        if (score >= beta):
-            return score
-        if (score > bestscore):
-            bestscore = score
-        if (score > alpha):
-            alpha = score
-    return bestscore
-
-def quiesce( alpha, beta, board):
-    stand_pat = evaluate_board(board)
-    if( stand_pat >= beta ):
-        return beta
-    if( alpha < stand_pat ):
-        alpha = stand_pat
-
-    for move in board.legal_moves:
-        if board.is_capture(move):
+    def alphabeta(self,alpha, beta, depthleft,board):
+        bestscore = -9999
+        if (depthleft == 0):
+            return self.quiesce(alpha, beta,board)
+        if board.fen() in self.movehistory[depthleft]:
+            print("found in move history alpha beta")
+            return self.movehistory[depthleft][board.fen()]
+        for move in board.legal_moves:
             board.push(move)
-            score = -quiesce( -beta, -alpha,board )
+            score = -self.alphabeta(-beta, -alpha, depthleft - 1,board)
             board.pop()
-
-            if( score >= beta ):
-                return beta
-            if( score > alpha ):
+            if (score >= beta):
+                return score
+            if (score > bestscore):
+                bestscore = score
+            if (score > alpha):
                 alpha = score
-    return alpha
+        self.movehistory[depthleft][board.fen()] = bestscore
+        return bestscore
 
-def selectmove(depth,board):
+    def quiesce(self,alpha, beta, board):
+        stand_pat = self.evaluate_board(board)
+        if( stand_pat >= beta ):
+            return beta
+        if( alpha < stand_pat ):
+            alpha = stand_pat
 
-    bestMove = chess.Move.null()
-    bestValue = -99999
-    alpha = -100000
-    beta = 100000
-    for move in board.legal_moves:
-        board.push(move)
-        boardValue = -alphabeta(-beta, -alpha, depth-1,board)
-        if boardValue > bestValue:
-            bestValue = boardValue
-            bestMove = move
-        if( boardValue > alpha ):
-            alpha = boardValue
-        board.pop()
-    movehistory.append(bestMove)
-    return bestMove
+        for move in board.legal_moves:
+            if board.is_capture(move):
+                board.push(move)
+                score = -self.quiesce( -beta, -alpha,board )
+                board.pop()
+
+                if( score >= beta ):
+                    return beta
+                if( score > alpha ):
+                    alpha = score
+        return alpha
+
+    def selectmove(self,board):
+
+        bestMove = chess.Move.null()
+        bestValue = -99999
+        alpha = -100000
+        beta = 100000
+        if board.fen() in self.movehistory[self.depth] :
+            print("found in move history")
+            return self.movehistory[self.depth][board.fen()]
+        for move in board.legal_moves:
+            board.push(move)
+            boardValue = -self.alphabeta(-beta, -alpha, self.depth-1,board)
+            if boardValue > bestValue:
+                bestValue = boardValue
+                bestMove = move
+            if( boardValue > alpha ):
+                alpha = boardValue
+            board.pop()
+        self.movehistory[self.depth][board.fen()] = bestMove
+        with open(f'moveHistoryDepth.pickle', 'wb') as handle:
+            pickle.dump(self.movehistory, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
+        return bestMove
     
-movehistory =[]
+    def getMoveHistory(self):
+        return self.movehistory
+
+if __name__ == "__main__":
+    depth = 3
+    # create move history file if it doesn't exist
+    if not os.path.isfile(f'moveHistoryDepth.pickle'):
+        with open(f'moveHistoryDepth.pickle', 'wb') as handle:
+            moveHistory = {}
+            for i in range(1,depth+1):
+                moveHistory[i] = {}
+            pickle.dump(moveHistory, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    else :
+        with open(f'moveHistoryDepth.pickle', 'rb') as handle:
+            moveHistory = pickle.load(handle)
+            for i in range(1,depth+1):
+                if i not in moveHistory:
+                    moveHistory[i] = {}
+
+    board = chess.Board()
+    chessAI = ChessMinMax(depth)
+    print(chessAI.getMoveHistory())
+    while not board.is_game_over():
+        print(board)
+        move = chessAI.selectmove(board)
+        board.push(move)
+    print(board)
